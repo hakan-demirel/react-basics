de796e27874532c5c9d8bb33013e4199edc13090
Hakan Demirel <mrhakandmrl@gmail.com>
2022-05-03 18:56:36 +0300

    Mini book project.

--- src/index.js
+++ src/index.js
@@ -3,16 +3,39 @@ import ReactDom from "react-dom";
 
 //Nested components, react tools
 
-function Greeting() {
+function BookList() {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section>
+      <Book />
+    </section>
   );
 }
 
-const Person = () => <h2>Hakan Demirel</h2>;
-const Message = () => <p>This is my message</p>;
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+const Image = () => {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg"
+      alt=""
+    />
+  );
+};
+
+const Title = () => {
+  return <h1>I Love You to the Moon and Back</h1>;
+};
+
+const Author = () => {
+  return <h4>Amelia Hepworth</h4>;
+};
+
+ReactDom.render(<BookList />, document.getElementById("root"));
