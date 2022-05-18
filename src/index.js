11f91c81c7150c05ea3ea6d69b30cca3cc26c301
Hakan Demirel <mrhakandmrl@gmail.com>
2022-05-18 14:27:42 +0300

    JSX Css

--- src/index.js
+++ src/index.js
@@ -42,7 +42,11 @@ const Title = () => {
 };
 
 const Author = () => {
-  return <h4>Amelia Hepworth</h4>;
+  return (
+    <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
+      Amelia Hepworth
+    </h4>
+  );
 };
 
 ReactDom.render(<BookList />, document.getElementById("root"));
