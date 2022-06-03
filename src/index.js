9d922231172e840f6701266aca6b74d1ae6a2add
Hakan Demirel <mrhakandmrl@gmail.com>
2022-06-03 11:35:07 +0300

    Proper list

--- src/index.js
+++ src/index.js
@@ -5,46 +5,33 @@ import ReactDom from "react-dom";
 import "./index.css";
 
 // Setup vars
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg",
-  title: "I Love You to the Moon and Back",
-  author: "Amelia Hepworth",
-};
-const secondBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/518E56QKfQL._SX367_BO1,204,203,200_.jpg",
-  title: "Our Class is a Family",
-  author: " Shannon Olsen",
-};
 
+const books = [
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg",
+    title: "I Love You to the Moon and Back",
+    author: "Amelia Hepworth",
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/518E56QKfQL._SX367_BO1,204,203,200_.jpg",
+    title: "Our Class is a Family",
+    author: " Shannon Olsen",
+  },
+];
+
+const names = ["john", "peter", "susan"];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>;
+});
+console.log(newNames);
 function BookList() {
-  return (
-    <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
-          aperiam earum aut inventore maxime magnam deleniti, facilis recusandae
-          neque modi!
-        </p>
-      </Book>
-
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-    </section>
-  );
+  return <section className="booklist">{newNames}</section>;
 }
 const Book = (props) => {
-  const { img, title, author, children } = props;
+  const { img, title, author } = props;
   return (
     <article className="book">
       <img src={img} alt="" />
-      {children}
       <h1>{title}</h1>
       <h4>{author}</h4>
     </article>
